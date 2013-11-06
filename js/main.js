de560df7c232d3fc91cca05110510ef947376b2f
piotrl88 <alucard.lisek@gmail.com>
2013-11-06 23:32:19 +0100

    Update 06.11.2013 23:30
    Added some styles.
    Correct validator method.
    Try to push fixtures into model for each route.

--- js/main.js
+++ js/main.js
@@ -5,14 +5,7 @@ var App = Em.Application.create({
     LOG_STACKTRACE_ON_DEPRECATION: true,
     LOG_VERSION: true,
     LOG_ACTIVE_GENERATION: true,
-    TestField:  Ember.TextField.extend({
-        attributeBindings: ['name', "value"],
-        isValid: function() {
-            console.log(this.get('value') + "|" + this.get("name"));
-            return ($.trim(this.get('value')).toLowerCase() === $.trim(this.get("data-answer")).toLowerCase());
-        }.property('value'),
-        classNameBindings: 'isValid:valid:invalid'
-    })
+    TestField : Ember.TextField.extend({})
 });
 
 App.Router.map(function () {
@@ -42,56 +35,66 @@ App.IndexController = Em.ArrayController.extend({
         // Setup each controller with its own model
         this.controllerFor('ex1').set('model', model);
         this.controllerFor('ex2').set('model', model);
-    },
-    actions: {
     }
 });
+
 
 App.Ex1 = DS.Model.extend({
     name : DS.attr("string"),
     pict: DS.attr("string"),
+    isValid: DS.attr(),
     answer: DS.attr("string")
 });
 App.Ex2 = DS.Model.extend({
-    pict: DS.attr("string"),
-    answer: DS.attr("string")
+    name : DS.attr("string"),
+    pict2: DS.attr("string"),
+    isValid: DS.attr(),
+    answer: DS.attr("string"),
+    desc : DS.attr("string")
 });
+
 
 App.Ex1.FIXTURES = [
     {
         id: 1,
-        name : "",
-        pict: "foggy.jpg",
+        name : "fogg",
+        pict: "img/exercise1/1.png",
+        isValid : null,
         answer: "foggy"
     },
     {
         id: 2,
-        name : "",
-        pict: "raining.jpg",
+        name : "raining",
+        pict: "img/exercise1/2.png",
+        isValid : null,
         answer: "raining"
     },
     {
         id: 3,
-        name : "",
-        pict: "cloudy.jpg",
-        answer: "cloudy"
-    },
-    {
-        id: 4,
-        name : "",
-        pict: "sunny.jpg",
+        name : "sunny",
+        pict: "img/exercise1/3.png",
+        isValid : null,
         answer: "sunny"
     },
     {
+        id: 4,
+        name : "cloudy",
+        pict: "img/exercise1/4.png",
+        isValid : null,
+        answer: "cloudy"
+    },
+    {
         id: 5,
-        name : "",
-        pict: "windy.jpg",
+        name : "windy",
+        pict: "img/exercise1/5.png",
+        isValid : null,
         answer: "windy"
     },
     {
         id: 6,
-        name : "",
-        pict: "snowing.jpg",
+        name : "snowing",
+        pict: "img/exercise1/6.png",
+        isValid : null,
         answer: "snowing"
     }
 ];
@@ -99,33 +102,51 @@ App.Ex1.FIXTURES = [
 App.Ex2.FIXTURES = [
     {
         id: 1,
-        pict: "foggy.jpg",
-        answer: "foggy"
+        name : "0",
+        pict: "img/exercise2/1.png",
+        answer: "1",
+        isValid : null,
+        desc : "doing a project"
     },
     {
         id: 2,
-        pict: "raining.jpg",
-        answer: "raining"
+        name : "2",
+        pict: "img/exercise2/2.png",
+        answer: "2",
+        isValid : null,
+        desc : "enjoying a field trip"
     },
     {
         id: 3,
-        pict: "cloudy.jpg",
-        answer: "cloudy"
+        name : "3",
+        pict: "img/exercise2/3.png",
+        answer: "3",
+        isValid : null,
+        desc : "working on computers"
     },
     {
         id: 4,
-        pict: "sunny.jpg",
-        answer: "sunny"
+        name : "4",
+        pict: "img/exercise2/4.png",
+        answer: "4",
+        isValid : null,
+        desc : "taking a test"
     },
     {
         id: 5,
-        pict: "windy.jpg",
-        answer: "windy"
+        name : "5",
+        pict: "img/exercise2/5.png",
+        answer: "5",
+        isValid : null,
+        desc : "giving a presentation"
     },
     {
         id: 6,
-        pict: "snowing.jpg",
-        answer: "snowing"
+        name : "6",
+        pict: "img/exercise2/6.png",
+        answer: "6",
+        isValid : null,
+        desc : "practicing yoga"
     }
 ];
 
@@ -136,34 +157,69 @@ App.IndexRoute = Ember.Route.extend({
 });
 
 App.IndexEx1Controller = Ember.ArrayController.extend({
+    checked : false,
     needs: "index",
     actions : {
-        checkAnswers : function(model) {
-            console.log(this.store.find('ex1'));
-            console.log("value: "+model.get('content')[0].get('name'));
-            console.log("answer: "+model.get('content')[0].get('answer'));
-
-             /*var store = App.get('store.ex1');
-             var answers = this.get('answers');
-             var ans = answers.name;
-             console.log("#"+ans);*/
-
+        checkAnswers : function(model, checked) {
+            //var validFlag = true;
+            $("form").addClass("validation");
+            $.each(model.get('content'), function( index, value ) {
+                if ($.trim(value.get("name")).toLowerCase() !== value.get("answer") ) {
+                    value.set("isValid", false);
+                } else {
+                    value.set("isValid", true);
+                }
+            });
         }
     }
 });
 
-App.IndexEx1Route = Ember.Route.extend({
-    model: function() {
-        return this.modelFor('index');
+App.IndexEx2Controller = Ember.ArrayController.extend({
+    needs: "index",
+    actions : {
+        checkAnswers : function(model) {
+            $("form").addClass("validation");
+            $.each(model.get('content'), function( index, value ) {
+                if ($.trim(value.get("name")).toLowerCase() !== value.get("answer") ) {
+                    value.set("isValid", false);
+                } else {
+                    value.set("isValid", true);
+                }
+            });
+        }
     }
 });
 
-App.IndexEx2Controller = Ember.ArrayController.extend({
-    needs: "index"
+
+App.IndexEx1Route = Ember.Route.extend({
+    model: function() {
+        //console.log(App.Ex1.FIXTURES);
+        //console.log(this.modelFor('index').get('content'));
+        //this.modelFor('index').set('content', App.Ex1.FIXTURES);
+        return this.modelFor('index');
+        //return this.modelFor('index');
+    }
 });
 
 App.IndexEx2Route = Ember.Route.extend({
     model: function() {
-        return this.modelFor('index');
+        //return this.modelFor('index');
+        return App.Ex2.FIXTURES;
+        //return this.modelFor('index');
     }
 });
+
+/*Ember.Handlebars.registerBoundHelper("hints", function(model) {
+    var theArray = model.get('content').content;
+    var len = theArray.length;
+    var i = len;
+    while (i--) {
+        var p = parseInt(Math.random()*len);
+        var t = theArray[i];
+        console.log(theArray[i].get('answer'));
+        theArray[i] = theArray[p];
+        theArray[p] = t;
+    }
+});*/
+
+
